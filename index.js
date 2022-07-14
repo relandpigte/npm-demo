5d1a81ec37bbf7889d766d7f6d20fc1d897822b9
Reland Pigte <reland102088@gmail.com>
2022-07-14 16:41:42 +0800

    epxress library

--- index.js
+++ index.js
@@ -1,1 +1,78 @@
-var _ = require('underscore')
+const express = require('express')
+const { restart } = require('nodemon')
+const Joi = require('joi')
+const app = express()
+
+app.use(express.json())
+
+const courses = [
+  { id: 1, name: 'Reland Pigte' },
+  { id: 2, name: 'Marilyn Dagum' },
+]
+
+// Validation
+function validateCourse(course) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  })
+
+  return schema.validate(course)
+}
+
+app.get('/api/courses', (req, res) => {
+  res.send(courses)
+})
+
+app.get('/api/courses/:id', (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id))
+  if (!course)
+    return res.status(404).send('The course with the given ID was not found')
+
+  res.send(course)
+})
+
+app.post('/api/courses', (req, res) => {
+  const { error } = validateCourse(req.body)
+
+  if (error) return res.status(400).send(error.details[0].message)
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  }
+
+  courses.push(course)
+  res.send(course)
+})
+
+app.put('/api/courses/:id', (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id))
+  if (!course) {
+    return res.status(404).send('The course with the given ID was not found')
+  }
+
+  const { error } = validateCourse(req.body)
+  if (error) {
+    return res.status(400).send(error.details[0].message)
+  }
+
+  course.name = req.body.name
+  res.send(course)
+})
+
+app.delete('/api/courses/:id', (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id))
+  if (!course) {
+    return res.status(404).send('The course with the given ID was not found')
+  }
+
+  const index = courses.indexOf(course)
+  courses.splice(index, 1)
+  res.send(course)
+})
+
+// Listen
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
